f4d70f4a055fb96ee5c14c54c101d424d85973d3
Owen <owal7428@colorado.edu>
2021-11-17 20:27:34 -0700

    Update main.cpp
    Added beginning argument checks in main function

--- main.cpp
+++ main.cpp
@@ -15,6 +15,22 @@ using namespace std;
 
 int main (int argc, char* argv[])
 {
-    cout << "Hello There!" << endl;
+    if (argc != 3)
+    {
+        cout << "Invalid number of arguments." << endl;
+        cout << "Usage: ./<program name> <csv file> <hashTable size>" << endl;
+
+        return 0;
+    }
+
+    ifstream file (argv[1]);
+
+    if (file.fail())
+    {
+        cout << "Failed to open the file." << endl;
+
+        return 0;
+    }
+
     return 0;
 }
