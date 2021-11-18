bd70c9cc53d0eb71c47fe0517da7180ef117a802
Owen Allison <owal7428@colorado.edu>
2021-11-18 15:15:07 -0700

    Update ProfBST.cpp
    Added the method to add a new node into the BST

--- ProfBST.cpp
+++ ProfBST.cpp
@@ -10,16 +10,16 @@
 
 using namespace std;
 
-//Does basically nothing, not even gonna waste my time on fancy comments
+//Does basically nothing, not even gonna waste my time on this one
 ProfBST::ProfBST()
 {
-    root = NULL;
+    root = NULL;    //Seriously, one line?!
 }
 
 //-----------------------------------------------------------------------------
-//~ProfBST:     Self deletion method
-//Parameters:   None
-//Returns:      Nothing
+//~ProfBST:         Self deletion method
+//Parameters:       None
+//Returns:          Nothing
 //-----------------------------------------------------------------------------
 //Why am I even doing this for one line of code?!?
 
@@ -30,9 +30,9 @@ ProfBST::~ProfBST()
 
 
 //-----------------------------------------------------------------------------
-//deleteAll:    Helper function for ~ProfBST
-//Parameters:   Pointer to professor object
-//Returns:      Void
+//deleteAll:        Helper function for ~ProfBST
+//Parameters:       Pointer to professor object
+//Returns:          Void
 //-----------------------------------------------------------------------------
 
 void deleteAll(Professor* node)
@@ -43,9 +43,54 @@ void deleteAll(Professor* node)
     delete node;                //Deletes node (yeeet)!
 }
 
+
+//-----------------------------------------------------------------------------
+//addProfessor:     Adds a new node to the BST
+//Parameters:       Two string variables
+//Returns:          Void
+//-----------------------------------------------------------------------------
+
 void ProfBST::addProfessor(string profId, string profName)
 {
+    if (searchProfessor(profId))    //Checks to see if the professor already exists in the tree
+        return;                     //Exits function if it does
+    
+    Professor *newProfessor = new Professor();  //Creates the new Professor object
 
+    newProfessor -> profName = profName;        //Sets all of the variables for the new object
+    newProfessor -> profId = profId;
+    newProfessor -> right = nullptr;
+    newProfessor -> left = nullptr;
+    
+    if (!root)                  //Checks if the root node exists or not
+        root = newProfessor;    //If it does not, it makes the root equal to the new node
+    
+    else
+        addHelper(newProfessor, root);      //Recursive loop to place new object in the correct position
+    
+    
+}
+//-----------------------------------------------------------------------------
+//addHelper:        Searches for the correct place to insert new node
+//Parameters:       Node to be inserted, Node that is being checked
+//Returns:          Void
+//-----------------------------------------------------------------------------
+
+void addHelper(Professor *&node, Professor *root)
+{
+    if (root == nullptr)        //If the current node being searched is null, insert here
+    {
+        root = node;
+        return;
+    }
+    
+    if (node -> profId > root -> profId)    //Checks if the current node should go left
+        addHelper(node, root -> right);     //Or right of current root
+    
+    else if (node -> profId < root -> profId)
+        addHelper(node, root -> left);
+    
+    return;
 }
 
 Professor* ProfBST::searchProfessor(string profId)
