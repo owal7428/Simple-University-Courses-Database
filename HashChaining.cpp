cc8a6fdfeb6d1523c76a0fbbc3cefcc23534a4ed
Owen <owal7428@colorado.edu>
2021-11-19 19:34:10 -0700

    Update HashChaining.cpp
    Beginning work on HashChaining

--- HashChaining.cpp
+++ HashChaining.cpp
@@ -8,19 +8,38 @@
 
 using namespace std;
 
+//Constructor
 HashChaining::HashChaining(int size)
 {
+    hashTableSize = size;   //Sets value of hashTableSize
 
+    hashTable = new Course*[hashTableSize]; //Sets the size of hashTable
 }
 
+//Deconstructor
 HashChaining::~HashChaining()
 {
+    profDb.~ProfBST();  //Deletes the BST of professors
+
+    for (int i = 0; i < hashTableSize; i++)     //Loops through index of the hashTable
+    {
+        Course *temp = hashTable[i];
+        
+        while (temp != nullptr)     //Deletes linked list in every index
+        {
+            Course *next = temp -> next;
+            delete temp;
+            temp = next;
+        }
+    }
+
+    delete[] hashTable;     //Deletes array of pointers
 
 }
 
 int HashChaining::hash(int courseNumber)
 {
-
+    return courseNumber % hashTableSize;
 }
 
 void HashChaining::bulkInsert(string filename)
