b794bc46d648f2ca3f387b3b701747c79962044d
Owen <owal7428@colorado.edu>
2021-11-20 17:12:09 -0700

    Update HashOpenAddressing.cpp
    Copy/paste easy functions over from HashChaining.cpp

--- HashOpenAddressing.cpp
+++ HashOpenAddressing.cpp
@@ -35,10 +35,18 @@ void HashOpenAddressing::search(int courseYear, int courseNumber, string profId)
 
 void HashOpenAddressing::displayAllCourses()
 {
-
+    for (int i = 0; i < hashTableSize; i++) //Loops through index of the hashTable
+    {
+        Course *temp = hashTable[i];
+        displayCourseInfo(temp);
+    }
 }
 
 void HashOpenAddressing::displayCourseInfo(Course* c)
 {
-	
+	if (c == nullptr)
+        cout << "Course was not found." << endl;
+    
+    else
+        cout << c -> year << " " << c -> courseName << " " << c -> courseNum << " " << c -> prof -> profName << endl;
 }
