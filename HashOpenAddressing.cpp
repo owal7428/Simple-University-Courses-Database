8878a4c6b48eea690681fac1eae55a6c098bb2ad
owal7428 <owal7428@colorado.edu>
2021-12-05 12:12:12 -0700

    Fixed quadratic probing algorithm
    It now gives the correct result

--- HashOpenAddressing.cpp
+++ HashOpenAddressing.cpp
@@ -96,11 +96,11 @@ void HashOpenAddressing::bulkInsert(string filename)
             for (int i = 1; i < hashTableSize; i++) //Implementation of quadratic probing
             {
                 numSearches++;
-                int newIndex = hash(index + i * i); //Finds new index to check
+                index = hash(index + i * i); //Finds new index to check
 
-                if (hashTable[newIndex] == nullptr) //Checks if new index is empty
+                if (hashTable[index] == nullptr) //Checks if new index is empty
                 {
-                    hashTable[newIndex] = newCourse;
+                    hashTable[index] = newCourse;
                     break;
                 }
             }
